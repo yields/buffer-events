cf93e1a77daaa5d7bd3455f3f41cd37634528db9
Amir Abushareb <yields@icloud.com>
2014-06-27 16:14:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+/**
+ * Expose `buffer`
+ */
+
+module.exports = buffer;
+
+/**
+ * Buffer `emitter`.
+ *
+ * @param {EventEmitter} emitter
+ * @return {Function}
+ * @api public
+ */
+
+function buffer(emitter){
+  var emit = emitter.emit;
+  var buf = [];
+
+  emitter.emit = function(){
+    buf.push(arguments);
+    return this;
+  };
+
+  return function(_){
+    while (_ = buf.shift()) {
+      emit.apply(emitter, _);
+    }
+  };
+}
